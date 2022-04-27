37cdc157c59186a13e7b3d7faa627cc168581e30
xpchild <jianwei.zhao@alipay.com>
2022-04-27 14:40:34 +0800

    [Bugfix] deallocate tcn when resize BP
    Former-commit-id: 1b1412d5b73a47ea4487e458cc83156423aedc1a

--- storage/innobase/buf/buf0buf.cc
+++ storage/innobase/buf/buf0buf.cc
@@ -2273,6 +2273,7 @@ withdraw_retry:
           mutex_free(&block->mutex);
           rw_lock_free(&block->lock);
 
+          lizard::deallocate_block_tcn(block);
           ut_d(rw_lock_free(&block->debug_latch));
         }
 
